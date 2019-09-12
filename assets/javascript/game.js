308ed776833cb43e6fcb1ec81fbc54f33083a546
Niq Schwartz <niqschwartz@gmail.com>
2019-09-11 23:49:13 -0500

    basic HTML layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+// var random_result;
+// var lost;
+// var win;
+
+// random_result = Math.floor(Math.random()*101)+19;
+
+// console.log(random_result);
+
+// $("#result").html('Random Result: ');
+
+// for (var i =0; i < 4; i++){
+//     var ramdom = Math.floor(Math.random() *12);
+
+//     var crystal = $("<div>");
+//     crystal.attr({
+//         "class": 'crystal',
+//         "data-random": random
+//     });
+
+//     $("#crystals").append(crystal);
+// }
+
+
+
+var wins =0;
+var losses= 0;
+var currentTotal = 0;
+var randomNumber;
+
+alert ("ITS WORKING");
+
+
+randomNumber = Math.floor(Math.random() *101) + 19;
+
+console.log(randomNumber);
+
+
