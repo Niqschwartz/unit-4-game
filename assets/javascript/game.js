68c3814eb4268ff91c5ab3e37d93eb26e0ad9000
Niq Schwartz <niqschwartz@gmail.com>
2019-09-11 23:56:34 -0500

    trying to debug

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,19 @@
-// var random_result;
-// var lost;
-// var win;
-
-// random_result = Math.floor(Math.random()*101)+19;
-
-// console.log(random_result);
-
-// $("#result").html('Random Result: ');
-
-// for (var i =0; i < 4; i++){
-//     var ramdom = Math.floor(Math.random() *12);
-
-//     var crystal = $("<div>");
-//     crystal.attr({
-//         "class": 'crystal',
-//         "data-random": random
-//     });
-
-//     $("#crystals").append(crystal);
-// }
-
-
 
 var wins =0;
 var losses= 0;
 var currentTotal = 0;
 var randomNumber;
 
-alert ("ITS WORKING");
+// WHY ISN'T ANYTHING WORKING?!?
 
+alert ("WORK!!!!");
 
+$("button-1").click(
+    console.log("clicked")
+);
+
+// computer's generated random number 20-120
 randomNumber = Math.floor(Math.random() *101) + 19;
-
 console.log(randomNumber);
 
 
